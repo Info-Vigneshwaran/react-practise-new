9ec53576cd7f342504fd89fd13da934f25fd852e
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-19 16:16:51 +0530

    Simple counter functionality added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Counter() {
+  const [counter, setCounter] = useState(0);
   return (
     <>
-      <p>sdfjghjh</p>
+      <p>Counter {counter}</p>
+      <button
+        onClick={(e) => {
+          setCounter(counter + 1);
+        }}
+      >
+        INC
+      </button>
+
+      <button
+        onClick={(e) => {
+          setCounter(counter - 1);
+        }}
+      >
+        DEC
+      </button>
     </>
   );
 }
