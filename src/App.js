97e6e81bba4934d3a50a811ebfcbc7ea35dd7298
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-19 16:13:30 +0530

    Basic Route Functionalities added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Home from './components/Home';
+import Counter from './components/Counter';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  useHistory,
+  Link,
+} from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="counter">Counter</Link> |
+      </nav>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="counter" element={<Counter />} />
+      </Routes>
+    </Router>
   );
 }
