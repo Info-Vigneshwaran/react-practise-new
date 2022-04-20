2373d031a7b36d80d437f6a74f4911061e86f5d3
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-04-20 11:23:06 +0530

    Multi API Calls

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
 export default function Posts() {
-  const [type, setType] = useState('');
+  const [type, setType] = useState('posts');
+  const [api_data, setApiData] = useState([]);
+  const api_url = `https://jsonplaceholder.typicode.com/`;
+  useEffect(() => {
+    let new_api_url = `${api_url}${type}`;
+    fetch(new_api_url).then(async (response) => {
+      let result = await response.json();
+      setApiData(result);
+    });
+  }, [type]);
+  const handleBtnClick = (newType) => {
+    setType(newType);
+  };
   return (
     <>
-      <button>Posts</button>
-      <button>Comments</button>
-      <button>Users</button>
+      <p>Type {type}</p>
+      <button onClick={(e) => handleBtnClick('posts')}>Posts</button>
+      <button onClick={(e) => handleBtnClick('comments')}>Comments</button>
+      <button onClick={(e) => handleBtnClick('users')}>Users</button>
+      <p>{JSON.stringify(api_data)}</p>
     </>
   );
 }
